4de36b4c952275afd43fcefa1102385a8a12aa7c
itsaeed0 <ashourisaeid78@gmail.com>
2024-11-07 22:30:06 +0330

    icons are installed

---
+++ app.js
@@ -0,0 +1,6 @@
+import { library, icon } from '@fortawesome/fontawesome-svg-core'
+import { faCamera } from '@fortawesome/free-solid-svg-icons'
+
+library.add(...all)
+
+const camera = icon({ prefix: 'fas', iconName: 'camera' })
